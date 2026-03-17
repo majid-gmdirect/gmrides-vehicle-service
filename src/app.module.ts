43f3b96154264bed1bfca02dc03713b844352786
Ahmad Joya <joya.abn@gmail.com>
2026-03-17 13:31:21 +0430

    fdsa

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
 
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './auth/roles.guard';
-import { CustomerModule } from './customer/customer.module';
-import { DriverModule } from './driver/driver.module';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 @Module({
   imports: [
@@ -15,11 +12,9 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
       isGlobal: true,
       envFilePath: '.env',
     }),
-    UserModule,
+
     PrismaModule,
     AuthModule,
-    CustomerModule,
-    DriverModule,
   ],
   providers: [
     {
