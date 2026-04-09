bac7702dbcb320975368d062a899332c2661dc76
alizada-hadi <alizadahadi2050@gmail.com>
2026-04-09 10:49:13 +0430

    fixed the issue

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
 import { RolesGuard } from './roles.guard';
+import { RedisModule } from '../redis/redis.module';
 
 @Module({
-  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
-  providers: [RolesGuard],
-  exports: [PassportModule, RolesGuard],
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' }), RedisModule],
+  providers: [JwtStrategy, RolesGuard],
+  exports: [PassportModule, RolesGuard, JwtStrategy],
 })
 export class AuthModule {}
